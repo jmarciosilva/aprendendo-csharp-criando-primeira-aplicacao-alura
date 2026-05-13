b5c310a35c376e8ba11663dbd7f39972e14742e9
José Marcio Ferreira da Silva <jose.silva@guty.com.br>
2026-05-13 17:09:44 -0300

    Criando uma funcao para exibir mensagem de boas vindas

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Screen Sound
+
+string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+
+// criando uma função local para exibir a mensagem de boas vindas
+void ExibirMensagemDeBoasVindas()
+{
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+// chamando a função local para exibir a mensagem de boas vindas
+ExibirMensagemDeBoasVindas();
+
